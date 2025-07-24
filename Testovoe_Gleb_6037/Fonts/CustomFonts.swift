decb3c575bc0951ab8ad65ed1e46d079aa9d205c
Gleb <zabrodagleb77@gmail.com>
2025-07-24 20:25:20 +0300

    Add custom fonts

---
+++ Testovoe_Gleb_6037/Fonts/CustomFonts.swift
@@ -0,0 +1,22 @@
+//
+//  CustomFonts.swift
+//  Gleb_6037
+//
+//  Created by Gleb Zabroda on 20.07.2025.
+//  Copyright © 2025 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+enum CustomFonts: String {
+    case kaiseiTokumin = "KaiseiTokumin"
+    case poppins = "Poppins"
+}
+
+enum CustomFontStyle: String {
+    case regular = "-Regular"
+    case medium = "-Medium"
+    case bold = "-Bold"
+    case extraBold = "-ExtraBold"
+    case light = "-Light"
+}
