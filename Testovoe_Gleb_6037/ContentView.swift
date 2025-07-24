5333f6428560a01cd36a3a4c68a30ded08efb321
Gleb <zabrodagleb77@gmail.com>
2025-07-24 20:05:09 +0300

    Initial Commit

---
+++ Testovoe_Gleb_6037/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Testovoe_Gleb_6037
+//
+//  Created by Alex Fedula on 24.07.2025.
+//  Copyright © 2025 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Testovoe_Gleb_6037/Testovoe_Gleb_6037App.swift
@@ -0,0 +1,19 @@
+//
+//  Testovoe_Gleb_6037App.swift
+//  Testovoe_Gleb_6037
+//
+//  Created by Alex Fedula on 24.07.2025.
+//  Copyright © 2025 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct Testovoe_Gleb_6037App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
